bfa3a252551c1279965ee7eb43eeae7404b25848
dangkhoa1404 <khoadang.1404@gmail.com>
2024-02-23 09:08:23 +0700

    start App

---
+++ app/src/main/java/com/lutech/literaryapp/screen/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.lutech.literaryapp.screen.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lutech.literaryapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
